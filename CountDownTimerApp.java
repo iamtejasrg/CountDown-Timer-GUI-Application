4f712a7925f9551fe57a9733903b9a34d7c0810c
Tejas Gorde <95203031+iamtejasrg@users.noreply.github.com>
2023-12-15 09:44:51 +0530

    Update CountDownTimerApp.java

--- CountDownTimerApp.java
+++ CountDownTimerApp.java
@@ -4,7 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
+import java.awt.event.ActionEvent;  
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
